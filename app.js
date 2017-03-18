1223ff34c9faafd766469545ece4587dac10d1f5
ybinstock <ybinstock@gmail.com>
2017-03-18 18:38:44 -0400

    five

--- app.js
+++ app.js
@@ -5,26 +5,9 @@ AFRAME.registerComponent('cursor-listener', {
       document.querySelector('[light]').emit('fade2');
       document.querySelector(".black-sky").emit('fade');
       document.querySelector(".white-sky").emit('fade');
-
-
       document.querySelector('[sphere]').removeEventListener('cursor-listener', evt);
-
-      //document.querySelector('[sphere]').setAttribute( 'visible', 'false');
-
-      //document.querySelector(".black-sky").setAttribute( 'visibility', false);
-      //document.querySelector(".white-sky").setAttribute( 'visibility', true);
-
-      //
-      //var COLORS = ['red', 'green', 'blue'];
-      //var randomIndex = Math.floor(Math.random() * COLORS.length);
-      //document.querySelector('[light]').setAttribute( 'color', COLORS[randomIndex]);
-      //console.log('I was clicked at: ', evt.detail.intersection.point);
 
     });
 
   }
 });
-
-//https://aframe.io/docs/0.5.0/core/component.html#tick-time-timedelta
-
-//https://rawgit.com/ngokevin/kframe/csstricks/scenes/aincraft/components/random-color.js
